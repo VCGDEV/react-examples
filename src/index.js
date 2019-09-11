336936f18a1b5583974e92f9c6fe2b10fbaa6700
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-09-11 13:08:33 -0500

    basic testing for todos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Tab from "./components/tab/tab";
+import "./components/redux-basics/todos-test";
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Tab className="App" />, rootElement);
