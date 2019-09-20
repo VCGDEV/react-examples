c9b8fc981b45bcc934fb2b8b467a6f61167f73b0
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-09-20 11:41:05 -0500

    set initial state to store

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 const rootElement = document.getElementById("root");
 
+const persistedState = {
+  todos: [{ id: 0, text: "Welcome back", completed: false }]
+};
+
 // creating new store
-const store = createStore(fullApp);
+// second parameter will end in teh state and override reducer default value
+const store = createStore(fullApp, persistedState);
 
 ReactDOM.render(
   <Provider store={store}>
