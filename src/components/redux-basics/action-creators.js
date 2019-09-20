5b26bbefc361a15f6234e24e5b4c51e84b61a81d
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-09-20 12:53:08 -0500

    save and load state from local storage

--- src/components/redux-basics/action-creators.js
+++ src/components/redux-basics/action-creators.js
@@ -6,10 +6,11 @@ import {
   DECREMENT
 } from "./constants";
 
-let nextId = 0;
+import { v4 } from "node-uuid";
+
 export const addTodo = text => ({
   type: ADD_TODO,
-  id: nextId++,
+  id: v4(),
   text
 });
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ import Tab from "./components/tab/tab";
 import { fullApp } from "./components/redux-basics/reducers-index";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { loadState, saveState } from "./load-state";
+import throttle from "lodash/throttle";
+
 const rootElement = document.getElementById("root");
 
-const persistedState = {
-  todos: [{ id: 0, text: "Welcome back", completed: false }]
-};
+const persistedState = loadState();
 
 // creating new store
 // second parameter will end in teh state and override reducer default value
 const store = createStore(fullApp, persistedState);
+
+store.subscribe(
+  throttle(() => {
+    // save only todo list and not all state properties
+    saveState({
+      todos: store.getState().todos
+    });
+  }, 1000)
+);
 
 ReactDOM.render(
   <Provider store={store}>
