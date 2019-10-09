d619fd733124d5c7c2b1a522ef2a58b6f1015d96
Victor de la Cruz <vcg_cruz@hotmail.com>
2019-10-09 12:28:52 -0500

    wrap dispatch to log dispatch actions

--- src/configure-store.js
+++ src/configure-store.js
@@ -3,12 +3,32 @@ import { createStore } from "redux";
 import { loadState, saveState } from "./load-state";
 import throttle from "lodash/throttle";
 
+const addLoggingToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+  return action => {
+    console.group(action.type);
+    console.log(`%c prev state: `, `color: gray`, store.getState());
+    console.log(`%c action`, `color: blue`, action);
+    const returnValue = rawDispatch(action);
+    console.log(`%c next state`, `color: green`, store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
 
   // creating new store
   // second parameter will end in teh state and override reducer default value
   const store = createStore(fullApp, persistedState);
+
+  if (process.env.NODE_ENV !== "production") {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(
     throttle(() => {
